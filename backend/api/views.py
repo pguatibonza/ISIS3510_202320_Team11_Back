8cd85abb7483e780308cbd1fd420c1b5e9a80b8f
Juan David Díaz <juanddiaz13@gmail.com>
2023-10-03 10:04:26 -0500

    assigns trail to trip. verifies trail exists and is available.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import User,Trip,Load,Trailer,AccessPoint
+from rest_framework.response import Response
+from rest_framework import status
 
-from .serializers import LoadSerializer,TrailerSerializer,AccessPointSerializer,TripSerializer
+
+from .serializers import LoadSerializer,TrailerSerializer,AccessPointSerializer,TripSerializer,AssignTripTrailerSerializer
 # Create your views here.
 # GET POST UPDATE DELETE
 # class UserGetCreate(generics.ListCreateAPIView):
@@ -43,3 +46,34 @@ class TripGetCreate(generics.ListCreateAPIView):
 class TripUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
+
+class AssignTripTrailer(generics.UpdateAPIView):
+    queryset = Trip.objects.all()
+    serializer_class = AssignTripTrailerSerializer
+    lookup_url_kwarg = 'trip_id'
+
+    def update(self, request, *args, **kwargs):
+        trip = self.get_object()
+        serializer = AssignTripTrailerSerializer(data=request.data)
+        if serializer.is_valid():
+            # Valid trailer_id from the serializer
+            trailer_id = serializer.validated_data['trailer_id']
+
+            try:
+                # Check if the trailer exists
+                trailer = Trailer.objects.get(pk=trailer_id)
+            except Trailer.DoesNotExist:
+                return Response({'detail': 'Trailer not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+            # Assign the trailer to the trip
+            trip.trailer = trailer
+            trip.save()
+
+            #Changes Trailer status
+            trailer.status = Trailer.TrailerStatus.INTRANSIT
+            trailer.save()
+
+            return Response({'detail': f'Trailer {trailer_id} assigned to trip {trip.id}.'}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
